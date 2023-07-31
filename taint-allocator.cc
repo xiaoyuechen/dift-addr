a7c0da162abd03946628e5c40c4f68c8a5a2d71f
Xiaoyue Chen <xchen@vvvu.org>
2023-07-31 14:04:53 +0800

    Add taint allocator

---
+++ taint-allocator.cc
@@ -0,0 +1,34 @@
+/*
+ * clueless --- Characterises vaLUEs Leaking as addrESSes
+ * Copyright (C) 2023  Xiaoyue Chen
+ *
+ * This file is part of clueless.
+ *
+ * clueless is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * clueless is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with clueless.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "taint-allocator.h"
+
+#include <algorithm>
+
+namespace clueless
+{
+
+taint
+fifo_taint_allocator::alloc ()
+{
+  return taint{ (t_ + 1) % taint::N };
+}
+
+}
---
+++ taint-allocator.h
@@ -0,0 +1,40 @@
+/*
+ * clueless --- Characterises vaLUEs Leaking as addrESSes
+ * Copyright (C) 2023  Xiaoyue Chen
+ *
+ * This file is part of clueless.
+ *
+ * clueless is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * clueless is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with clueless.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TAINT_ALLOCATOR_H
+#define TAINT_ALLOCATOR_H
+
+#include "taint.h"
+
+namespace clueless
+{
+
+class fifo_taint_allocator
+{
+public:
+  taint alloc ();
+
+private:
+  taint t_{};
+};
+
+}
+
+#endif
